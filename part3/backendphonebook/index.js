516119960955672f50841b1dadbb7b697caafc19
esteban <estebanguerra1990@gmail.com>
2025-05-14 14:26:30 +0200

    backendphonebook
    fdavf

---
+++ part3/backendphonebook/index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const persons = [    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }]
+
+app.get('/', (request, response) => {
+
+    response.json(persons)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (!person) {
+    return response.status(404).json({error:"no se ha encontrado nigun titular con ese identificador"})
+  }
+  response.status(200).json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (!person) {
+     return response.status(404).json({error:"no se ha encontrado nigun titular con ese identificador"})
+  }
+
+  const newPersons = persons.filter(person => person.id !== id)
+
+  persons.length = 0
+  console.log(persons);
+  
+  persons.push(...newPersons)
+    console.log(persons);
+
+  response.status(200).json({response:"persona eliminada correctamente", agenda: persons})
+})
+
+app.post('/api/persons', (request, response) => {
+  const {name,number} = request.body
+    
+    if (!name || !number) {
+     return response.status(404).json({error:"name or number empty"})
+    }
+    const contact = persons.find(person => person.name === name)
+    console.log(contact);
+    
+    if (contact) {
+      return response.status(404).json({error:"name must be unique"})
+    }
+
+    let counter = persons.length>0?Math.max(...persons.map(person=>person.id)):0
+    const personAdd =  {id: Number(counter+1),name:name, number: number}
+    persons.push(
+     personAdd
+    )
+    console.log(persons);
+    
+    
+
+     response.status(200).json({response:"personas anyadidas correctamente", personAdd})
+  
+})
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send("<p>Phonebook has info for 2 people</p>"+date)
+  
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
